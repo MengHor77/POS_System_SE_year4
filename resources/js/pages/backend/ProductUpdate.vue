b9d109480f0db8781aa0af48ebc6e57b5e2e8bed
meng hor <piseth125125@gmail.com>
2026-01-18 22:57:10 +0700

    add file  controller and model in admin

---
+++ resources/js/pages/backend/ProductUpdate.vue
@@ -0,0 +1,16 @@
+<template>
+    <BackendLayout>
+        <div class="text-center mt-10">
+            <h1 class="text-4xl font-bold text-primary">Product Update Page</h1>
+        </div>
+    </BackendLayout>
+</template>
+<script lg="ts">
+import { defineComponent } from "vue";
+import BackendLayout from "../../layouts/BackendLayout.vue";
+
+export default defineComponent({
+    name: "ProductUpdate",
+    components: { BackendLayout },
+});
+</script>
--- resources/js/router/index.ts
+++ resources/js/router/index.ts
@@ -2,10 +2,23 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AdminLogin from '../pages/backend/AdminLogin.vue';
 import AdminDashboard from '../pages/backend/Dashboard.vue';
 import Home from '../pages/frontend/Home.vue';
+import ProductUpdate from '../pages/backend/ProductUpdate.vue';
+import Inventory from '../pages/backend/Inventory.vue';
+import PurchaseOrder from '../pages/backend/PurchaseOrder.vue';
+import Profile from '../pages/backend/Profile.vue';
+import Report from '../pages/backend/Report.vue';   
+import Cashier from '../pages/backend/Cashier.vue';
+import Notification
+
+
+
+from '../pages/backend/Notification.vue';
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/admin/login', name: 'admin-login', component: AdminLogin },
   { path: '/admin/dashboard', name: 'admin-dashboard', component: AdminDashboard },
+  { path: '/admin/product/product-update', name: 'admin-product-product-update', component: ProductUpdate },
+
 ];
 
 const router = createRouter({
