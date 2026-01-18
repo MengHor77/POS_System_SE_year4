5d3021b8207884c8904ecb77318eaff50e91f52c
meng hor <piseth125125@gmail.com>
2026-01-18 12:54:54 +0700

    fx: layout

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,13 +1,19 @@
 <template>
-    <header class="bg-white shadow px-6 py-4  pb-5 flex justify-between items-center">
+    <header
+        class="bg-white shadow px-6 py-4 pb-5 flex justify-between items-center"
+    >
         <h1 class="text-xl font-bold text-primary">POS Admin</h1>
 
-        <button
-            class="bg-danger text-white px-4 py-2 rounded hover:bg-red-600 transition"
-            @click="logout"
-        >
-            Logout
-        </button>
+        <div class="flex gap-3">
+            <h1 class="mt-2 font-bold">Admin</h1>
+            <button
+                class="border border-dark px-4 py-2 rounded hover:bg-danger transition flex items-center gap-2"
+                @click="logout"
+            >
+                <i class="fas fa-sign-out-alt"></i>
+                Logout
+            </button>
+        </div>
     </header>
 </template>
 
