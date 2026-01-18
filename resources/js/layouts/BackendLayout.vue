09e734a0e0aa67dfc18ad445ff62a3dd00728228
meng hor <piseth125125@gmail.com>
2026-01-18 10:38:56 +0700

    fix format

--- resources/js/layouts/BackendLayout.vue
+++ resources/js/layouts/BackendLayout.vue
@@ -1,23 +1,20 @@
 <template>
-  <div class="flex min-h-screen bg-bgMain">
-    <!-- Sidebar -->
-    <Sidebar
-      :collapsed="collapsed"
-      @toggle="toggleSidebar"
-    />
+    <div class="flex min-h-screen bg-bgMain">
+        <!-- Sidebar -->
+        <Sidebar :collapsed="collapsed" @toggle="toggleSidebar" />
 
-    <!-- Main Content -->
-    <div
-      class="flex-1 flex flex-col transition-all duration-300"
-      :class="collapsed ? 'ml-20' : 'ml-64'"
-    >
-      <Header @toggle-sidebar="toggleSidebar" />
+        <!-- Main Content -->
+        <div
+            class="flex-1 flex flex-col transition-all duration-300"
+            :class="collapsed ? 'ml-20' : 'ml-64'"
+        >
+            <Header @toggle-sidebar="toggleSidebar" />
 
-      <main class="flex-1 p-6">
-        <slot />
-      </main>
+            <main class="flex-1 p-6">
+                <slot />
+            </main>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -26,16 +23,16 @@ import Header from "../components/Header.vue";
 import Sidebar from "../components/Sidebar.vue";
 
 export default defineComponent({
-  name: "BackendLayout",
-  components: { Header, Sidebar },
-  setup() {
-    const collapsed = ref(false);
+    name: "BackendLayout",
+    components: { Header, Sidebar },
+    setup() {
+        const collapsed = ref(false);
 
-    const toggleSidebar = () => {
-      collapsed.value = !collapsed.value;
-    };
+        const toggleSidebar = () => {
+            collapsed.value = !collapsed.value;
+        };
 
-    return { collapsed, toggleSidebar };
-  },
+        return { collapsed, toggleSidebar };
+    },
 });
 </script>
