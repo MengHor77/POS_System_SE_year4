55e611f257b2d2e322442b64e8fa2bc64c5975d7
meng hor <piseth125125@gmail.com>
2026-01-18 22:16:21 +0700

    fix: homepage frontend

--- resources/js/router/index.ts
+++ resources/js/router/index.ts
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import AdminLogin from '../pages/backend/AdminLogin.vue';
 import AdminDashboard from '../pages/backend/Dashboard.vue';
-
+import Home from '../pages/frontend/Home.vue';
 const routes = [
+  { path: '/', name: 'home', component: Home },
   { path: '/admin/login', name: 'admin-login', component: AdminLogin },
   { path: '/admin/dashboard', name: 'admin-dashboard', component: AdminDashboard },
 ];
