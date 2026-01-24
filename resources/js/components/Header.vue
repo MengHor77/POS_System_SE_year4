336cbdb9527858caa43eb1306b572e940c949f12
meng hor <piseth125125@gmail.com>
2026-01-24 18:15:33 +0700

    fix: cs

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,13 +1,17 @@
 <template>
-    <header
-        class="bg-white shadow px-6 py-4 flex justify-between items-center"
-    >
-        <h1 class="text-xl font-bold text-primary">Admin Dashboard!</h1>
+    <header class="bg-white shadow px-6 py-4 flex justify-between items-center">
+        <h1 class="text-3xl font-bold text-primary flex items-center gap-2">
+            <i class="fas fa-boxes"></i> Admin Dashboard
+        </h1>
 
-        <div class="flex gap-3">
-            <h1 class="mt-3 font-bold">Admin</h1>
+        <div class="flex items-center gap-4">
+            <div class="flex items-center gap-2">
+                <i class="fas fa-user-circle text-gray-500 text-xl"></i>
+                <span class="font-bold">Admin</span>
+            </div>
+
             <button
-                class="border border-dark px-4 py-2 rounded hover:bg-danger transition flex items-center gap-2"
+                class="border border-dark px-4 py-2 rounded hover:bg-red-500 hover:text-white hover:border-red-500 transition flex items-center gap-2"
                 @click="logout"
             >
                 <i class="fas fa-sign-out-alt"></i>
