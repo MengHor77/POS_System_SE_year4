b5b3b012dd52ff3086fba0e1b41b6326a7cb17d6
meng hor <piseth125125@gmail.com>
2026-01-31 11:20:06 +0700

    fix: cs

--- resources/js/pages/backend/Category/Index.vue
+++ resources/js/pages/backend/Category/Index.vue
@@ -11,7 +11,7 @@
                         @click="showCreate = true"
                         class="mb-4 bg-dark text-white px-4 py-2 rounded hover:bg-darkSoft"
                     >
-                        Add Category
+                        <i class="fas fa-plus"></i> Add Category
                     </button>
                 </div>
                 <!-- Filter/Search -->
